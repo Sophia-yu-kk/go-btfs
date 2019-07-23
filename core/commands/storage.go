96cd275828e0216aba4b916225b412dba8fc9198
Shelly Wei <guaichen07@gmail.com>
2019-07-22 17:52:39 -0700

    upload with channel created

--- core/commands/storage.go
+++ core/commands/storage.go
@@ -1,9 +1,17 @@
 package commands
 
 import (
+	"context"
 	"fmt"
-	cmds "github.com/ipfs/go-ipfs-cmds"
 	"strings"
+
+	"github.com/TRON-US/go-btfs/core/commands/cmdenv"
+	"github.com/TRON-US/go-btfs/core/ledger"
+	ledgerPb "github.com/TRON-US/go-btfs/core/ledger/pb"
+
+	cmds "github.com/ipfs/go-ipfs-cmds"
+	//libcrypto "github.com/libp2p/go-libp2p-core/crypto"
+	ic "github.com/libp2p/go-libp2p-crypto"
 )
 
 const (
@@ -53,17 +61,65 @@ var storageUploadCmd = &cmds.Command{
 		}
 		return nil
 	},
-	Run: func(req *cmds.Request, emitter cmds.ResponseEmitter, environment cmds.Environment) error {
-		for _, hash := range req.Arguments {
-			err := emitter.Emit(hash)
+	Run: func(req *cmds.Request, emitter cmds.ResponseEmitter, env cmds.Environment) error {
+		fileHash := req.Arguments[0]
+		price, _ := req.Options[uploadPriceOptionName].(int64)
+		// get self key pair
+		n, err := cmdenv.GetNode(env)
+		if err != nil {
+			return err
+		}
+		selfPubKey, err := n.Identity.ExtractPublicKey()
+		if err != nil {
+			return fmt.Errorf("fail to extract self peer ID's public key: %v", err)
+		}
+		selfPrivKey := n.PrivateKey
+		// get other node's public key as address and create channel between them
+		var channelID *ledgerPb.ChannelID
+		for _, str := range req.Options[hostSelectionOptionName].([]string) {
+			_, pid, err := ParsePeerParam(str)
 			if err != nil {
-				return err
+				return fmt.Errorf("failed to parse peer address '%s': %s", str, err)
+			}
+			peerPubKey, err := pid.ExtractPublicKey()
+			channelID, err = initChannel(selfPubKey, selfPrivKey, peerPubKey, price)
+			if err != nil {
+				continue
+			}
+			if channelID != nil {
+				break
 			}
 		}
-		price, _ := req.Options[uploadPriceOptionName].(int64)
-		replica, _ := req.Options[replicationFactorOptionName].(int64)
-		mode, _ := req.Options[hostSelectModeOptionName].(string)
-		list, _ := req.Options[hostSelectionOptionName].([]string)
-		return emitter.Emit(fmt.Sprintf("price %d, replica %d, mode %s, list %s", price, replica, mode, list))
+		// TODO: Remote call other Node with fileHash
+
+		//replica, _ := req.Options[replicationFactorOptionName].(int64)
+		//mode, _ := req.Options[hostSelectModeOptionName].(string)
+		return emitter.Emit(fmt.Sprintf("channel ID: %d \n file-hash: %s", channelID.GetId(), fileHash))
 	},
+}
+
+func initChannel(payerPubKey ic.PubKey, payerPrivKey ic.PrivKey, recvPubKey ic.PubKey, amount int64) (*ledgerPb.ChannelID, error) {
+	ctx := context.Background()
+	// build connection with ledger
+	clientConn, err := ledger.LedgerConnection()
+	if err != nil {
+		log.Error("fail to connect", err)
+		return nil, err
+	}
+	// new ledger client
+	ledgerClient := ledger.NewClient(clientConn)
+	// create account
+	_, err = ledger.ImportAccount(ctx, payerPubKey, ledgerClient)
+	if err != nil {
+		log.Error("fail to create account with peer ID", err)
+		return nil, err
+	}
+	// prepare channel commit and sign
+	cc := ledger.NewChannelCommit(payerPubKey, recvPubKey, amount)
+	sig, err := ledger.Sign(payerPrivKey, cc)
+	if err != nil {
+		log.Error("fail to sign channel commit with private key", err)
+		return nil, err
+	}
+	return ledger.CreateChannel(ctx, ledgerClient, cc, sig)
 }
